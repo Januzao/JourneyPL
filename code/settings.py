e1dd7ad3c11cb2c321b856b5246984bd075e9bcc
Yevhen Oleinichenko <eu.oleynich@gmail.com>
2025-06-14 22:20:24 +0200

    Added README.md file and settings parameters.

---
+++ code/settings.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+# tile size in used to precision
+TILE_SIZE = 64
+
+ANIMATION_SPEED = 5
+PICKUP_RADIUS = 100
+
+# Paths
+# Parent path
+PARENT_DIR = Path(__file__).parent.parent
+# Tile sprites
+SPRITES_DIR = PARENT_DIR / 'data' / 'graphics' / 'sprites'
+# Objects folder
+OBJECTS_DIR = PARENT_DIR / 'data' / 'graphics' / 'objects'
+# UI elements
+UI_DIR = PARENT_DIR / 'data' / 'graphics' / 'ui'
+# Player sprites and animations
+PLAYER_DIR = PARENT_DIR / 'images' / 'player'
+# Maps and tilesets
+MAPS_DIR = PARENT_DIR / 'data' / 'maps'
+TILESETS_DIR = PARENT_DIR / 'data' / 'tilesets'
