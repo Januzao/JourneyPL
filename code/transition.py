c2b543cc77132970d4a8b2fd9c8a95512e7cbf06
Yevhen Oleinichenko <eu.oleynich@gmail.com>
2025-06-26 15:20:19 +0200

    bugfix and changed animations

--- code/transition.py
+++ code/transition.py
@@ -5,48 +5,23 @@ class HorizontalRectangleSwipeTransition(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-
         self.width, self.height = pygame.display.get_window_size()
-
-        self.image = pygame.Surface((self.width, self.height)).convert_alpha()
+        self.image = pygame.Surface((self.width, self.height * 1.5)).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
-
+        self.alpha = 0
+        self.image.fill((0, 0, 0, self.alpha))
         self.transitioning = True
-        self.speed = 300
-
-        # pass this through via the constructor so we can modify it to be any number
-        rect_count = 4
-
-        self.rectangles = []
-        division = self.height // rect_count
-
-        for i in range(rect_count):
-            self.rectangles.append(
-                pygame.Rect(0, division * (i), self.width, division)
-            )
-
-        self.to_the_right_x = 0
-        self.to_the_left_x = 0
 
     def update(self, dt):
-        self.image.fill((0, 0, 0, 0))
-        self.to_the_left_x -= self.speed * dt
-        self.to_the_right_x += self.speed * dt
-
-        for i, rect in enumerate(self.rectangles):
-            pygame.draw.rect(self.image, (255, 255, 255), rect)
-
-            if i % 2 == 0:
-                rect.move_ip(self.to_the_right_x, 0)
-            else:
-                rect.move_ip(self.to_the_left_x, 0)
-
         self.check_for_transition_complete()
+        self.image.fill((0, 0, 0, self.alpha))
+        if self.alpha <= 240:
+            self.alpha += 5
 
     def check_for_transition_complete(self):
-        if self.rectangles[0].left > self.width:
+        if self.alpha > 240:
             self.transitioning = False
 
     def is_transitioning(self):
